c21657ce3a9c76619256731ebe7c530c3c7d82d5
Shamir Duran <shamirduran@M110-COL.local>
2026-02-19 09:20:47 -0500

    flicker;

--- ex_target_affinity.js
+++ ex_target_affinity.js
@@ -1,17 +1,69 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Se agrega dentro del loaded para que espera a la carga de los elementos
+import React, { useEffect } from 'react';
+import { chatbotAddon } from './src/adapter/addons/chatbot';
 
-  window.alloy("sendEvent", {
-    renderDecisions: true,
-    type: "web.webinteraction.pageViews",
-    decisionScopes: ["__view__"],
-    xdm: {},
-    data: {
-      __adobe: {
-        target: {
-          "user.categoryId": "electronics",
-        },
-      },
-    },
-  });
-});
+const TargetWrapper = ({ element, props }) => {
+  const { location } = props;
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !window.alloy) return;
+
+    const applyTarget = () => {
+      if (!window.lastPropositions?.length) return;
+
+      try {
+        window.alloy('applyPropositions', {
+          propositions: window.lastPropositions.map((prop) => ({
+            ...prop,
+            renderAttempted: false,
+          })),
+        });
+      } catch (e) {
+        console.error('Error reaplicando Target:', e);
+      }
+    };
+    applyTarget();
+
+    if (!window.lastPropositions?.length) {
+      window.alloy('sendEvent', { renderDecisions: true }).then((result) => {
+        if (result.propositions) {
+          console.log(result);
+        }
+      });
+    }
+
+    const targetNode = document?.body;
+    let frameId;
+
+    const observer = new MutationObserver((mutations) => {
+      const hasNewNodes = mutations.some((m) => m.addedNodes.length > 0);
+      if (hasNewNodes) {
+        if (frameId) cancelAnimationFrame(frameId);
+        frameId = requestAnimationFrame(() => {
+          applyTarget();
+        });
+      }
+    });
+
+    observer.observe(targetNode, { childList: true, subtree: true });
+
+    // Limpieza al desmontar
+    return () => {
+      observer.disconnect();
+      if (frameId) cancelAnimationFrame(frameId);
+    };
+  }, [location.pathname]);
+  return element;
+};
+
+const wrapPageElement = ({ element, props }) => {
+  return <TargetWrapper element={element} props={props} />;
+};
+
+const onRouteUpdate = () => {
+  chatbotAddon();
+};
+
+export default {
+  onRouteUpdate,
+  wrapPageElement,
+};
